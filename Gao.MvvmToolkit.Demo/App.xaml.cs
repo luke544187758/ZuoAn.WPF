569386a5a1d2c33907b8522f128f280e25de4e51
luke544187758 <luke544187758@outlook.com>
2024-07-04 02:51:30 +0800

    添加项目文件。

---
+++ Gao.MvvmToolkit.Demo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Gao.MvvmToolkit.Demo.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+namespace Gao.MvvmToolkit.Demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public new static App Current=>(App)Application.Current;
+
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            //注册服务
+
+            //注册视图模型
+            services.AddScoped<MainViewModel>();
+            return services.BuildServiceProvider();
+        }
+    }
+
+}
